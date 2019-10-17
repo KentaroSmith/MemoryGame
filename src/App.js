65b4bbddec95849e43f9150b1e8f09b6421b0e8a
Dan Smith <daniel.smith8795@gmail.com>
2019-10-17 18:34:47 -0500

    adjusted file structure for components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import './App.css';
-import Card from "./components/card/index";
-
+import Card from "./components/card";
+import Nav from "./components/nav";
 function App() {
   return (
     <div className="App">
+      <Nav />
       <header className="App-header">
         <p>
           Welcome to my memory game! Try to only click each photo once!
         </p>
+        <Card />
       </header>
-      <div component={Card}></div>
     </div>
   );
 }
---
+++ src/components/pic1/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+//import photo from "./test.jpg";
+
+function Pic1() {
+    return (
+        <div className="col-md-2">
+            <div className="card">
+
+                {/* <img className="card-img" src={logo}>
+                </img> */}
+            </div>
+        </div>
+
+    );
+}
+
+export default Pic1;
